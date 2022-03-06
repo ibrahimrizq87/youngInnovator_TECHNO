9d761ef3608085d97ca4bd4bf488a0f85789dfa8
Ibrahim Rizq <ibrahimrizqabd@gmai.com>
2022-03-06 18:16:13 +0200

    second session

--- project/project.ino
+++ project/project.ino
@@ -5,13 +5,18 @@ int value=0;
 int ir1=7;
 int ir2=6;
   int count=0;
+  int buzzer=1;
+  int led=8;
 void setup() {
   lcd.begin(16, 2);
 pinMode(ir1,INPUT);
+pinMode(buzzer,OUTPUT);
 pinMode(ir2,INPUT);
+pinMode(led,OUTPUT);
 }
 
 void loop() {
+  
   if( digitalRead (ir1)==HIGH)
   {count++;}
   
@@ -20,14 +25,30 @@ lcd.setCursor(0, 0);
 lcd.print("No of people ");
 
  if( digitalRead (ir2)==HIGH)
-  {count--;}
+  {count--;
+  if (count<0){count=0;}}
   lcd.setCursor(0, 1);
 lcd.print(count);
-delay(1500);
+delay(1000);
+if (count>15)
+{tone(buzzer,1000);}
+else {noTone(buzzer);}
+
+
+if (value<300&& count >0){
+  digitalWrite(led,HIGH);
+}else{digitalWrite(led,LOW);}
+if (count >10 && count<=15 ){tone (buzzer,1000);
+delay(500);
+noTone(buzzer);}
+
+
+}
+
 /*
 if (value>400) {lcd.setCursor(0, 1);
 lcd.print("LIGHT Is on"); }
 else {lcd.setCursor(0, 1);
 lcd.print("it is  Dark  ");}
   */
-  }
+  
